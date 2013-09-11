47bab4a87d122c29c1077789a1509c9620826b7b
Mark Lorenz <markjlorenz@dapplebeforedawn.com>
2013-09-10 21:27:50 -0400

    Better help

--- lib/options.rb
+++ lib/options.rb
@@ -1,11 +1,9 @@
 module Options
-  HELP = "Usage: #{__FILE__} [options]"
   def self.get
     return @o if @o
     @o = Struct.new(:ref_voltage, :ref_pounds, :ref_tare, :hide_count).new
     OptionParser.new do |opts|
-      opts.banner = HELP
-      opts.on("-h", "--help", "This help") { puts HELP; exit }
+      opts.on("-h", "--help", "This help")       { exec "more #{__FILE__}" }
 
       opts.on("-e", "--ref_voltage [VOLTS]", "") { |arg| @o.ref_voltage = arg.to_f }
       opts.on("-w", "--ref_pounds [POUNDS]", "") { |arg| @o.ref_pounds  = arg.to_f }
